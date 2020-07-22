9ee16ed548316bf6d96357554cb720c307dcab8f
Nazar <nazardovhanchuk@gmail.com>
2020-07-23 01:52:31 +0300

    Updates

---
+++ precache-manifest.01f80e8b8bab54cccfb101350eacae96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676283dcdb30ba187bec7016d8eb3664",
+    "url": "./index.html"
+  },
+  {
+    "revision": "385f0ad6736a49dd963b",
+    "url": "./static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "f070ca5082d2f94752df",
+    "url": "./static/js/2.c565d8bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c565d8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385f0ad6736a49dd963b",
+    "url": "./static/js/main.bd8f68ab.chunk.js"
+  },
+  {
+    "revision": "0c165204287428b65967",
+    "url": "./static/js/runtime-main.a37292e9.js"
+  }
+]);
